92083635c20f01ddebfcb4b28c1bcde8fbd67c8f
kalKun24 <grenouille24hi@gmail.com>
2023-12-26 15:14:38 +0900

    feat: :sparkles: #infomationモデルの管理画面表記変更

--- infomation/admin.py
+++ infomation/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import Infomation
 
-admin.site.register(Infomation)
+class InfomationAdmin(admin.ModelAdmin):
+    list_display = ["title", "autohr", "updated_at"]
+    list_filter = ["autohr"]
+    search_fields = ["title", "autohr"]
+    ordering = ["created_at", "updated_at", "autohr"]
+    list_per_page = 20
+
+admin.site.register(Infomation, InfomationAdmin)
