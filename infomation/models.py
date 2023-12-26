07ba8cbe4c16c233f341217cefdd33f9f16ea98c
kalKun24 <grenouille24hi@gmail.com>
2023-12-26 15:10:14 +0900

    feat: :sparkles: #infomationモデルの表示名変更

--- infomation/models.py
+++ infomation/models.py
@@ -11,16 +11,17 @@ class Infomation(models.Model):
             ('competition', '競技情報'),
             ('maintenance', 'メンテナンス')
         ),
-        default='infomation'
+        default='infomation',
+        verbose_name="カテゴリ"
     )
-    title = models.CharField(max_length=50)
-    message = models.TextField()
+    title = models.CharField(max_length=50, verbose_name="タイトル")
+    message = models.TextField(verbose_name="お知らせ内容")
     
-    autohr = models.ForeignKey(CustomUser, on_delete=models.CASCADE, to_field="username")
+    autohr = models.ForeignKey(CustomUser, on_delete=models.CASCADE, to_field="username", verbose_name="投稿者")
     
     # 日時
-    created_at = models.DateTimeField(auto_now_add=True) #データ挿入時
-    updated_at = models.DateTimeField(auto_now=True) #データ更新時
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="作成日時") #データ挿入時
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="更新日時") #データ更新時
     
     def __str__(self):
         return self.title
