61d9454df6ba537f8804cfc443a73f6961140947
kalKun24 <grenouille24hi@gmail.com>
2023-12-26 15:07:23 +0900

    feat: :sparkles: #CustomUserモデルの表示名変更

--- accounts/models.py
+++ accounts/models.py
@@ -2,4 +2,4 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
-    total_score = models.IntegerField(default=0)
+    total_score = models.IntegerField(default=0, verbose_name="総合得点")
