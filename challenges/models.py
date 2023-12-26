55b1ea4eeb4a93f6453282af88cf4cb86032f0f8
kalKun24 <grenouille24hi@gmail.com>
2023-12-26 15:05:58 +0900

    feat: :sparkles: #Challengeモデルの表示名を変更

--- challenges/models.py
+++ challenges/models.py
@@ -17,22 +17,23 @@ CATEGORIES = (('WebExploitation', 'WebExploitation'),
 # 演習クラス(課題の集合):1
 class Exercise(models.Model):
     exercise_regex = RegexValidator(regex=r'^#[0-9]{2}')
-    exercise = models.CharField(max_length=30, default='', validators=[exercise_regex])
-    disription = models.TextField(default='')
-    visible = models.BooleanField(default=False)
+    exercise = models.CharField(max_length=30, default='', validators=[exercise_regex], verbose_name="演習名")
+    disription = models.TextField(default='', verbose_name="説明")
+    visible = models.BooleanField(default=False, verbose_name="ユーザへの公開")
     
     category = models.CharField(
         max_length=30,
         choices = CATEGORIES,
-        default='Misc'
+        default='Misc',
+        verbose_name="カテゴリー"
     )
     
-    reference_url = models.URLField(default="")
-    explanation_url = models.URLField(default="")
+    reference_url = models.URLField(default="", verbose_name="教科書URL")
+    explanation_url = models.URLField(default="", verbose_name="解説URL")
     
     # 日時
-    created_at = models.DateTimeField(auto_now_add=True) #データ挿入時
-    updated_at = models.DateTimeField(auto_now=True) #データ更新時
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="作成日時") #データ挿入時
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="更新日時") #データ更新時
     
     def __str__(self):
         return self.exercise
@@ -42,31 +43,35 @@ class Exercise(models.Model):
     
 # 課題クラス:多
 class Challenge(models.Model):
-    identifier = models.CharField(max_length=50, unique=True, validators=[RegexValidator(regex='^[0-9a-zA-Z]*$')])
+    identifier = models.CharField(max_length=50, unique=True, validators=[RegexValidator(regex='^[0-9a-zA-Z]*$')], verbose_name="識別子")
     title_regex = RegexValidator(regex=r'^[0-9]{2}[.]{1}')
-    title = models.CharField(max_length=30, default='', validators=[title_regex])
-    exerciseName = models.ForeignKey(Exercise, on_delete=models.SET_DEFAULT, default='実戦問題')
+    title = models.CharField(max_length=30, default='', validators=[title_regex], verbose_name="タイトル")
+    exerciseName = models.ForeignKey(Exercise, on_delete=models.SET_DEFAULT, default='実戦問題', verbose_name="演習名")
     category = models.CharField(
         max_length=30,
         choices = CATEGORIES,
-        default='Misc'
+        default='Misc',
+        verbose_name="カテゴリー"
     )
-    problem = models.TextField(default='')
-    score = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(3000)], default=0)
-    flag = models.CharField(max_length=100, default='')
-    visible = models.BooleanField(default=False)
+    score = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(3000)], default=0, verbose_name="配点")
+    flag = models.CharField(max_length=100, default='', verbose_name="フラグ")
+    visible = models.BooleanField(default=False, verbose_name="ユーザへの公開")
+    
+    problem = models.TextField(default='', verbose_name="問題文")
     
     #ヒント
-    hint_one = models.CharField(max_length=100, blank=True, default="")
-    hint_two = models.CharField(max_length=100, blank=True, default="")
-    hint_three = models.CharField(max_length=100, blank=True, default="")
-    
-    # 日時
-    created_at = models.DateTimeField(auto_now_add=True) #データ挿入時
-    updated_at = models.DateTimeField(auto_now=True) #データ更新時
+    hint_one = models.CharField(max_length=100, blank=True, default="" , verbose_name="ヒント1")
+    hint_two = models.CharField(max_length=100, blank=True, default="", verbose_name="ヒント2")
+    hint_three = models.CharField(max_length=100, blank=True, default="", verbose_name="ヒント3")
     
     # 正解数
-    cleared_counts = models.IntegerField(default=0)
+    cleared_counts = models.IntegerField(default=0, verbose_name="正解者数")
+    
+    # 日時
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="作成日時") #データ挿入時
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="更新日時") #データ更新時
+    
+    
     
     def __str__(self):
         return self.title
@@ -76,10 +81,10 @@ class Challenge(models.Model):
         
 class Submit(models.Model):
     # 外部参照の理由：user削除時に点数が残ってしまうから。
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, to_field="username")
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, to_field="username", verbose_name="ユーザ")
     
     # 外部参照しない理由：問題が削除されたときに、点数情報も変わってしまうため。
-    identifier = models.CharField(max_length=50, validators=[RegexValidator(regex='^[0-9a-zA-Z]*$')],) 
+    identifier = models.CharField(max_length=50, validators=[RegexValidator(regex='^[0-9a-zA-Z]*$')], verbose_name="課題") 
     
     def __str__(self):
         submit_title = str(self.user) + ":" + str(self.identifier)
